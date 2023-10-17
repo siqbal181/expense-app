14434569972be5845e70dcbe7c0334066a95873e
Sidra Iqbal <sidra1360@gmail.com>
2023-10-17 10:50:41 +0100

    Navbar with useAuth0 hook

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import "./Navbar.css"
+import React from "react";
+import Button from "@mui/material/Button";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    const handleLogin = async () => {
+      await loginWithRedirect({
+        appState: {
+          returnTo: "/",
+        },
+      });
+    };
+
+    return (
+      <Button variant="contained" color="primary" onClick={handleLogin}>
+        Login
+      </Button>
+    );
+  };
+
+  const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    const handleLogout = () => {
+      logout({
+        logoutParams: {
+          returnTo: window.location.origin,
+        },
+      });
+    };
+
+    return (
+      <Button variant="contained" color="secondary" onClick={handleLogout}>
+        Logout
+      </Button>
+    );
+  };
+
   return (
     <div className="navbar">
-      <Button variant="contained" color="primary">Login</Button>
-      <Button variant="contained" color="secondary">Logout</Button>
+      <LoginButton />
+      <LogoutButton />
     </div>
   );
 };
