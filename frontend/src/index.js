25c31c51c952cc42547b0c343b06b8ee9453fdfb
Sidra Iqbal <sidra1360@gmail.com>
2023-10-17 10:31:31 +0100

    Added Auth0 config to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const authOConfig = {
+  domain: process.env.REACT_APP_AUTHO_DOMAIN,
+  clientId: process.env.REACT_APP_AUTHO_CLIENT_ID,
+  redirectUri: window.location.origin,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Auth0Provider
+        domain={authOConfig.domain}
+        clientId={authOConfig.clientId}
+        redirectUri={authOConfig.redirectUri}
+      >
+        <App />
+      </Auth0Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
 );
-
