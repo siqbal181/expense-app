875af73a3134113fd80508bd7921983a1c79ddc1
Sidra Iqbal <sidra1360@gmail.com>
2023-10-17 10:37:32 +0100

    Auth0 provider with navigate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,26 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
-
-const authOConfig = {
-  domain: process.env.REACT_APP_AUTHO_DOMAIN,
-  clientId: process.env.REACT_APP_AUTHO_CLIENT_ID,
-  redirectUri: window.location.origin,
-};
+import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <Auth0Provider
-        domain={authOConfig.domain}
-        clientId={authOConfig.clientId}
-        redirectUri={authOConfig.redirectUri}
-      >
+      <Auth0ProviderWithNavigate>
         <App />
-      </Auth0Provider>
+      </Auth0ProviderWithNavigate>
     </React.StrictMode>
   </BrowserRouter>,
 );
