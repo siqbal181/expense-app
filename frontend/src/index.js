8e234a29a129deb08993ed0137ff9d3de2496f07
Sidra Iqbal <sidra1360@gmail.com>
2023-10-26 14:04:42 +0100

    feat: Auth0 config changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+const authConfig = require('./auth_config.json')
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      {/* <Auth0ProviderWithNavigate> */}
-        <App />
-      {/* </Auth0ProviderWithNavigate> */}
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Auth0Provider
+    domain={authConfig.domain}
+    clientId={authConfig.clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      // audience: "https://dev-dkcsc3y8xpusyr42.us.auth0.com/api/v2",
+      // scope: "read:current_user update:current_user_metadata"
+    }}
+  >
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </Auth0Provider>
 );
+
