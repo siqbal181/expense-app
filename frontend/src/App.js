1e85b87342674cd04a8628cf53f53b1556f9ed65
Sidra Iqbal <sidra1360@gmail.com>
2023-11-04 18:55:47 +0000

    feat: router installed and monthly spend file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { CurrentBudgets } from './components/currentBudgetsTable/CurrentBudgets';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
+import MonthlySpends from './components/monthlySpends/MonthlySpends';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <CurrentBudgets/>
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/spends" element={<MonthlySpends />} /> 
+          <Route path="/" element={<CurrentBudgets />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
